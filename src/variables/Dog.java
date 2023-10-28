30dcbeb7b80649d4979204dc607297c9ec04eb43
prmcsep103 <primecommunity103@gmail.com>
2023-10-28 08:52:39 +0100

    Static Variables Example

---
+++ src/variables/Dog.java
@@ -0,0 +1,16 @@
+package variables;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class Dog {
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    public void printName() {
+        System.out.println("name = " + name);
+    }
+}

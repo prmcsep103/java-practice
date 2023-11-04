2060ae41d08e7b69fb139db63eb06556c9c16ec6
prmcsep103 <primecommunity103@gmail.com>
2023-11-04 12:02:29 +0000

    Challenge4 With Abstraction and Interface Example

---
+++ src/morningchallenge/PlayingCat.java
@@ -0,0 +1,29 @@
+package morningchallenge;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class PlayingCat {
+
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+        if (summer) {
+            if (temperature >= 25 && temperature <= 35) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (temperature >= 25 && temperature <= 35) {
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isCatPlaying(true, 10));
+        System.out.println(isCatPlaying(false, 36));
+        System.out.println(isCatPlaying(false, 35));
+    }
+}
---
+++ src/polymorphism/overriding/Parent.java
@@ -0,0 +1,14 @@
+package polymorphism.overriding;
+
+/**
+ * Created by Jay Vaghani
+ */
+public class Parent {
+    public void marry() {
+        System.out.println("Black Girl");
+    }
+
+    public static void staticExample() {
+        System.out.println("Parent class Static method");
+    }
+}
